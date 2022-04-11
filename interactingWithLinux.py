64a69b6afded14854355ccb6ecf6e978005ba09e
kmacosta2 <kmacosta2@miners.utep.edu>
2022-04-11 13:20:15 -0600

    starting to work with log files

---
+++ interactingWithLinux.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import re
+import subprocess
+#current_dir = os.getcwd()
+#get_traffic = subprocess.run(['cd','w_can/ICSim'])
+#loc = 'home/kali/w_can/ICSim/trafficForM8.log'
+
+f = open('trafficForM8', "r")
+print (f.read())
+f.close
+
+#identify how many different IDs exist?
+
+#def firstCommands():
+    #n = sys.platform
+#    os.chdir("/home/kali/Downloads/")
+#    print(current_dir)
+#    return ''
+
+#firstCommands()
