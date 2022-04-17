f207c30b916f78451b0d4ffbcad05d452e0aafed
kmacosta2 <kmacosta2@miners.utep.edu>
2022-04-16 19:15:36 -0600

    added handling known & unknown nodes

--- interactingWithLinux.py
+++ interactingWithLinux.py
@@ -1,41 +1,48 @@
-# -Identify how many different IDs exist in a given log file & their # of instances 
-# -When working with cangen(random) traffic lets identify 
-#       left/right turn signal--> ID 188#01...for left & ID 188#02...right
-#        & 20 mph--> ID 244#...followed by any value above 0 will register
-#        on the speedometer
-# traff_w_time recorded the following event: right signal(x2), left signal(x1),
-# then accel to 20 then drop to zero
-
+#
+# CS4310 SW1 Team 5 GGs
+l, r, mp = 'lt', 'rt', '20mph'
 knownIDs = {
-    'lt':'188#01',
-    'rt':'188#02',
-    '20mph':'244'
+    'lt': 0,
+    'rt': 0,
+    '20mph': 0
     }
-# the 10 bits that follow the mph would need to be above 0
 
 with open('traff_w_time', "r") as f:   # its in hex
-    packets_time = f.readlines()                  # >25k lines
+    packets_time = f.readlines()       # >25k lines
 with open('traff_rand_gen', "r") as f: # its in hex
-    packets_rand = f.readlines()                  # 20k lines
+    packets_rand = f.readlines()       # 20k lines
 
-def allNodesEncountered(logFile):
-    d = dict()
-    for l in logFile:
-        per_line = l.split(" ")
-        node = per_line[2][:3]
-        if node in d:# if already there, then simply update
-            d[node] = d[node] + 1
-        else: # otherwise, add it to the dictionary
-            d[node] = 1
-    for k in list(d.keys()):
-        print(f'Node ID: {k} ({d[k]})')
-    print(f'{len(d)} nodes encountered')
+"""
+    By default, this func allows you to collect all instances in a given log file per node and
+    collect the total nodes encountered in a dictionary. Alternatively, you could just 
+    acknowledge the known IDs only with param which='known'
+"""
+def nodesEncountered(logFile, le, r, mp, which='all'):
+    d = {}
+    if which == 'all':
+        for l in logFile:
+            per_line = l.split(" ")
+            node = per_line[2][:3]
+            if node in d:   # if already there, then simply update
+                d[node] = d[node] + 1
+            else:           # otherwise, add it to the dictionary
+                d[node] = 1
+        for k in list(d.keys()):
+            print(f'Node ID: {k} ({d[k]})')
+        print(f'{len(d)} nodes encountered')
+    elif which == 'known':
+        for l in logFile:
+            per_line = l.split(" ")
+            node = per_line[2][:14]
+            if node[:6] == '188#01':
+                knownIDs['lt'] += 1
+            elif node[:6] == '188#02':
+                knownIDs['rt'] += 1
+            elif node[:3] == '244' and node[5:14] != '0000000000':
+                knownIDs['20mph'] += 1
+        print(f'left turn blink: ({knownIDs[le]})\nright turn blink: ({knownIDs[r]})\n20 mph: ({knownIDs[mp]})')
 
-def identifyingTheSequence():
-    print('')
-    return ''
-
-#firstNode(packets_time)
-#firstNode(packets_rand)
-allNodesEncountered(packets_time)
-allNodesEncountered(packets_rand)
+nodesEncountered(packets_time, l, r, mp)
+#nodesEncountered(packets_rand, l, r, mp)
+nodesEncountered(packets_time, l, r, mp, which='known')
+#nodesEncountered(packets_rand, l, r, mp, which='known')
